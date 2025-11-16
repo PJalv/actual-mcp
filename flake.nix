86e2dd7e589154f6680f39882f2fe835f25385a0
PJalv <pjalvbusiness@gmail.com>
2025-11-15 18:05:24 -0800

    Add Nix flake for running MCP server with nix run

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Actual Budget MCP Server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "actual-mcp";
+        version = "1.5.0";
+        src = self;
+        buildInputs = [ pkgs.nodejs ];
+        buildPhase = ''
+          npm ci
+          npm run build
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp -r . $out
+          chmod +x $out/build/index.js
+          ln -s $out/build/index.js $out/bin/actual-mcp
+        '';
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/actual-mcp";
+      };
+    };
+}
